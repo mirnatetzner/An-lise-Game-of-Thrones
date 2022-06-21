1b75613bf32ed1e602077f60c4fa34c5d940c180
Larrissa Lira <100171322+larrissalira@users.noreply.github.com>
2022-06-21 20:14:29 -0300

    Banco de dados GOT + tabelas

---
+++ criação do banco GOT e das tabelas.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE got;
+
+create table house (
+house_id INT auto_increment NOT NULL primary key,
+house_name varchar (255),
+region varchar (255)
+);
+
+create table characters (
+character_id INT auto_increment NOT NULL primary key,
+character_name varchar (255),
+actor varchar (255),
+episodes_appeared INT NOT NULL,
+first_appearance varchar (4),
+last_appearance varchar (4)
+);
+
+create table episodes (
+episode_id INT auto_increment NOT NULL primary key,
+season varchar (2),
+episode tinyint,
+title varchar (100),
+release_date varchar (11),
+rating decimal (2,1),
+votes INT,
+summary varchar (400),
+writer_1 varchar (50),
+writer_2 varchar (50),
+star_1 varchar (50),
+star_2 varchar (50),
+star_3 varchar (50),
+users_reviews INT,
+critics_reviews INT,
+us_viewers decimal (4,2),
+duration tinyint,
+director varchar (50),
+budget_estimate tinyint
+);
+
+
